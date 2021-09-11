08ca6cf92f6ec61dab6351aa7087f28d33aa9dc5
Brian-Demon <BrianWMatejka@gmail.com>
2021-09-10 19:32:26 -0500

    add "logout" path for sessions#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # ROOT
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
 
   #OAUTH
   get "/auth/:provider/callback" => "sessions#create"
+  get "/logout" => "sessions#destroy", :as => :logout
 
 end
