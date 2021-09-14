4fffddde01cceeb1beccf7432ec48c42bcd6ac2f
Brian-Demon <BrianWMatejka@gmail.com>
2021-09-14 15:37:02 -0500

    `kernel_build.rb`: - Allow the new form partial to access attributes for:     - `kernel_configs`     - `kernel_sources`

--- app/models/kernel_build.rb
+++ app/models/kernel_build.rb
@@ -1,4 +1,6 @@
 class KernelBuild < ApplicationRecord
   has_many :kernel_configs
+  accepts_nested_attributes_for :kernel_configs
   has_many :kernel_sources
+  accepts_nested_attributes_for :kernel_sources
 end
