1e5500deb289a46514e0427157cdbfe441083a40
Brian-Demon <BrianWMatejka@gmail.com>
2021-09-17 19:48:03 -0500

    Add event listeners to application JS: - window: for page loading - newConfigButton: button for showing new config form - newSourceButton: button for showing new source form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,29 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+window.addEventListener("load", () => {
+  const newConfigButton = document.getElementById("show_new_config_form");
+  const newSourceButton = document.getElementById("show_new_source_form");
+  const sourceForm = document.getElementById("kernel_source_form");
+  const configForm = document.getElementById("kernel_config_form");
+
+  sourceForm.style.display = "none";
+  configForm.style.display = "none";
+
+  newConfigButton.addEventListener("click", (e) => {
+    if( configForm.style.display == "none"){
+      configForm.style.display = "block";
+    } else {
+      configForm.style.display = "none";
+    }
+  });
+
+  newSourceButton.addEventListener("click", (e) => {
+    if( sourceForm.style.display == "none"){
+      sourceForm.style.display = "block";
+    } else {
+      sourceForm.style.display = "none";
+    }
+  });
+});
